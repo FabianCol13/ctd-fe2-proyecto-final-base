0789bcd97bd727330f089561be207cfb7a8b9801
FabianCol <FABIANCOL13@GMAIL.COM>
2023-06-30 20:36:58 -0500

    Entrega Final

--- src/features/quote/Cita.test.tsx
+++ src/features/quote/Cita.test.tsx
@@ -30,71 +30,132 @@ const data = [
 const validQueries = data.find((q) => q.character)
 
 const handlers = [
-    rest.get(`${ API_URL }`, (req, res, ctx) => {
-        const character = req.url.searchParams.get("character");
-        if(character === null) {
-            return res(ctx.json([data[1]]), ctx.delay(200));
-        }
-        if(validQueries) {
-            return res(ctx.json([validQueries]));
-        }
-        return res(ctx.json([]), ctx.delay(200));
-    })
-];
+    // Correct the usage of the request handler
+    rest.get(`${API_URL}`, (req, res, ctx) => {
+      const character = req.url.searchParams.get("character");
+  
+      if (character === "fabian") {
+        return res(ctx.json([]), ctx.status(200));
+      }
+    return res(ctx.json(data));
+    }),
+  ];
 
-const server = setupServer(...handlers);
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+  const server = setupServer(...handlers);
 
-describe('Cita', () => {
-    describe("Cuando se apreta el btn aleatorio sin ningun nombre de personaje", () => {
-        it('Deberia mostrar la cita en la posicion 2', async () => {
-          render( <Cita /> );
-          const obtenerCitaButton = await screen.findByLabelText('Obtener cita aleatoria');
-          userEvent.click(obtenerCitaButton)
-          await waitFor(() => {
-            expect(screen.getByText("You're turning me into a criminal when all I want to be is a petty thug.")).toBeInTheDocument();
-          });
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+describe("Cita", () => {
+    describe("Cuando renderizamos el componente", () => {
+        beforeEach(() => {
+            render(<Cita />);
         });
-    })
+        it("No debería traer ninguna cita", async () => {
+            expect(screen.getByText(/No se encontro ninguna cita/i)).toBeInTheDocument();
+        });
+        it("El input de búsqueda debería estar vacío", async () => {
+            const input = screen.getByRole("textbox", { name: /Author Cita/i });
+            expect(input).toHaveValue('');
+        });
+        it("Debería haber un botón que diga: Obtener cita aleatoria", async () => {
+            expect(screen.getByRole("button", { name: /Obtener cita aleatoria/i })).toBeInTheDocument();
+        });
+    });
+    describe("Cuando hacemos clic en Obtener cita aleatoria", () => {
+        it("Mientras busca una cita, debería mostrar un mensaje de carga", async () => {
+            render(<Cita />);
 
-    describe("Cuando se coloca un personaje y darle al btn de obtener cita ", () => {
-        it('Me debe dar la cita correspondiente al personaje buscado y al darle al boton de obtener cita', async () => {
-            render( <Cita /> );
-            const input = screen.getByRole("textbox", { name: "Author Cita" });
-            const obtenerCitaButton = await screen.findByLabelText(/Obtener Cita/i);
-            await userEvent.click(input);
-            await userEvent.keyboard("Homer")
-            await userEvent.click(obtenerCitaButton);
+            const button = screen.getByRole("button", { name: /Obtener cita aleatoria/i });
+            userEvent.click(button);
+
             await waitFor(() => {
-              expect(screen.getByText("Oh, so they have Internet on computers now!")).toBeInTheDocument();
+                expect(screen.getByText(/CARGANDO/i)).toBeInTheDocument();
             });
-          });    
-    })
+        });
+    });
+    describe("Cuando ingresamos un personaje en el input", () => {
+        beforeEach(() => {
+            render(<Cita />);
+        });
+        it("El botón debe cambiar su texto a: Obtener Cita", async () => {
+            const input = screen.getByRole("textbox", { name: /Author Cita/i });
+            const button = await screen.findByRole("button", { name: /Obtener Cita/i });
 
-    describe("Cuando se le da al boton de borrar", () => {
-        it('Limpiar la cita y el personaje buscado cuando se le de al boton borrar', async () => {
-            render( <Cita /> );
-            const input = screen.getByRole("textbox", { name: "Author Cita" });
-            const obtenerCitaButton = await screen.findByLabelText(/Obtener Cita/i);
-            const borrarButton = screen.getByLabelText('Borrar');
-            await userEvent.click(input);
-            await userEvent.keyboard("Homer ")
-            userEvent.click(obtenerCitaButton);
+            userEvent.clear(input);
+            userEvent.click(input);
+            userEvent.keyboard("lisa");
+
             await waitFor(() => {
-                expect(screen.getByText("Oh, so they have Internet on computers now!")).toBeInTheDocument();
+                expect(button).toBeInTheDocument();
             });
+        });
+        it("Si el personaje es de Los Simpsons, debería mostrar una cita de ese personaje", async () => {
+            server.use(
+                rest.get(`${API_URL}?character=homer`, (req, res, ctx) => {
+                    return res(ctx.json(data), ctx.status(200));
+                })
+            )
 
-            userEvent.click(borrarButton);
+            const input = screen.getByRole("textbox", { name: /Author Cita/i });
+            const button = await screen.findByRole("button", { name: /Obtener Cita/i });
+
+            userEvent.clear(input);
+            userEvent.click(input);
+            userEvent.keyboard("homer");
+            userEvent.click(button);
+
+            await waitFor(() => {
+                expect(screen.getByText(data[0].character)).toBeInTheDocument();
+            });
+        });
+        it("Si ingreso un número o el personaje no es de Los Simpsons, debería mostrar un mensaje de error", async () => {
+            server.use(
+                rest.get(`${API_URL}?character=fabian`, (req, res, ctx) => {
+                    return res(ctx.json([]), ctx.status(200));
+                })
+            )
+
+            const input = screen.getByRole("textbox", { name: /Author Cita/i });
+            const button = await screen.findByRole("button", { name: /Obtener Cita/i });
+
+            userEvent.clear(input);
+            userEvent.click(input);
+            userEvent.keyboard("fabian");
+            userEvent.click(button);
+
+            await waitFor(() => {
+                expect(screen.getByText(/Por favor ingrese un nombre válido/i)).toBeInTheDocument();
+            });
+        });
+    });
+    describe("Cuando hacemos clic en el botón Borrar", () => {
+        beforeEach(() => {
+            render(<Cita />);
+        });
+        it("Debería limpiar el input para buscar personajes", async () => {
+            const input = screen.getByRole("textbox", { name: /Author Cita/i });
+            const button = screen.getByRole("button", { name: /Borrar/i });
+
+            userEvent.clear(input);
+            userEvent.click(input);
+            userEvent.keyboard("homer");
+            userEvent.click(button);
+
+            await waitFor(() => {
+                expect(input).toHaveValue('');
+            });
+        });
+        it("No debería traer ninguna cita", async () => {
+            const button = screen.getByRole("button", { name: /Borrar/i });
+            userEvent.click(button);
+
             await waitFor(() => {
                 expect(screen.getByText(/No se encontro ninguna cita/i)).toBeInTheDocument();
             });
-
-            await waitFor(() => {
-                expect(screen.getByDisplayValue("")).toBeInTheDocument();
-            });
         });
-    })
+    });
 });
+
